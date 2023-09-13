2e34b3a0ec423e230c9770ea7dc6f8e123e3f362
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2023-09-13 16:05:20 +0200

    Added plot

--- PreProject/main.py
+++ PreProject/main.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("data.csv") 
-print(df.head())
+df = pd.read_csv('data.csv')
+x = df.iloc[:, 0]
+y = df.iloc[:, 1]
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+line = slope * x + intercept
+
+plt.plot(x, y, 'o', x, line)
+plt.show()
